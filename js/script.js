acdadbe0118c51faf3d14a5f70a6a17bee4b3716
DavideIsaia <davide.enjoy@hotmail.it>
2022-04-27 15:03:15 +0200

    i numeri vengono cancellati alla fine del timer

--- js/script.js
+++ js/script.js
@@ -3,25 +3,41 @@
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
 // creo una funzione che stampa 5 numeri random (io scelgo un range da 1 a 100);
+const randomNumbers = document.getElementById("random-numbers");
+let rndNumber;
+
 for (let i =1; i <=5; i++) {
-    const rndNumber = getRndInteger(1, 100);
-    console.log(rndNumber);
+    rndNumber = getRndInteger(1, 100);
+    console.log(i, rndNumber);
+    randomNumbers.innerHTML += (` - ${rndNumber} - `);
 }
+
 
 // creo un timer che arrivato a zero fa scomparire i numeri e comparire i 5 prompt;
 const countContainer = document.getElementById("timer");
 let count = parseInt(countContainer.textContent);
-
-// Ogni secondo devo decrementare il count e scrivere il nuovo numero nel countContainer
 let userNumber;
+
 const timer = setInterval(function(){
     count--;
+    // quando il timer arriva a zero
     if (count === 0) {
+        // ferma il timer
         clearInterval(timer);
-        for (let i =1; i <=5; i++) {
-            userNumber = parseInt (prompt (`Inserisci il ${i} numero`));
-            console.log(userNumber);
-        }
+
+        // cancella i numeri a schermo
+        document.querySelector("h2").innerHTML = "";
+
+        // imposto un piccolo delay per il prompt, per dare il tempo ai numeri di venire cancellati
+        setTimeout(() => {
+            // chiedo 5 volte di inserire un numero finchè non viene inserito compreso nel range preimpostato
+            for (let i =1; i <=5; i++) {
+                do {
+                    userNumber = parseInt (prompt (`Inserisci il ${i}° numero compreso tra 1 e 100:`));
+                    console.log(`il ${i} numero è:`, userNumber);
+                } while (userNumber < 1 || userNumber > 100);
+            }             
+        }, 20);        
     } else {
         countContainer.innerHTML = count;
     }
